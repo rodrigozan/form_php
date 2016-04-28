dbb9593ea6a7d293141847c4ff0d1ca229c93199
rodrigozan <rodzandonadi@gmail.com>
2016-04-28 14:07:43 -0300

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -3,5 +3,7 @@ Nome: <input name="nome" type="text" placeholder="Digite seu Nome" required>
 <br>
 Email: <input name="email" type="text" placeholder="Digite seu Nome" required>
 <br>
+Senha: <input name="senha" type="password" placeholder="Digite sua Senha" required>
+<br>
 <button type="submit">Enviar</button>
 </form>
