c517fc8c0dc2dd6b384300fbfc6fa2930c5ae210
rodrigozan <rodzandonadi@gmail.com>
2016-04-28 14:06:11 -0300

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,7 @@
+<form method="post" action="inserir.php">
+Nome: <input name="nome" type="text" placeholder="Digite seu Nome" required>
+<br>
+Email: <input name="email" type="text" placeholder="Digite seu Nome" required>
+<br>
+<button type="submit">Enviar</button>
+</form>
